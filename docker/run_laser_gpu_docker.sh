6ebccc07f50af0d38d49829b1ca76c889019240f
Jay Jinhyung Park <jay@xl8.ai>
2020-03-11 21:39:47 -0700

    Remove cpu=true for GPU support

--- docker/run_laser_gpu_docker.sh
+++ docker/run_laser_gpu_docker.sh
@@ -5,5 +5,4 @@ GPU_NUMBER=$1
 docker run --rm -it --name="$(whoami)${GPU_NUMBER}" \
 	--gpus "device=${GPU_NUMBER}" \
 	-p 15000:80 \
-	xl8/laser-gpu:latest /bin/bash
-	#xl8/laser-gpu:latest python app.py
+	xl8/laser-gpu:latest python app.py
